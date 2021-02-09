e01255b052582162a3b100f975ceb47c73cccb1a
Felipe Azambuja <felipe@newbgp.com.br>
2021-02-09 20:05:31 -0300

    Primeiros testes

---
+++ src/FelipeAzambuja/Ciplus/JsonDataset.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace FelipeAzambuja\Ciplus;
+
+class JsonDataset extends \CodeIgniter\Controller {
+
+    /**
+     * 
+     * @var Model
+     */
+    private $model;
+
+    function setModel(Model $model) {
+        $this->model = $model;
+    }
+
+    function index($offset = 0,$buffer=10) {
+        
+    }
+
+    function delete($id) {
+        
+    }
+
+    function insert() {
+        
+    }
+
+    function update($id) {
+        
+    }
+
+}
